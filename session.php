95b376ea4a0ac49f3a95250314dc1d3d5c2d8e58
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-12 20:18:41 +0100

    Add files via upload

---
+++ session.php
@@ -0,0 +1,30 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+//or
+print_r($_SESSION);
+
+
+// remove all session variables
+session_unset();
+
+// destroy the session
+session_destroy(); 
+?>
+
+</body>
+</html> 
